f01b5fc4b346f0c38d134c4d5cee77f57edbfe2c
fufupatwo <aarond99@hotmail.com>
2023-12-20 18:55:56 -0600

    Merged T1 logo with black page.

---
+++ trendingdata/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import BlackPage from './components/BlackPage';
+import T1Logo from './components/T1Logo';
+
+function App() {
+  return (
+      <div className="App">
+        <BlackPage>
+            <T1Logo></T1Logo>
+        </BlackPage>
+      </div>
+  );
+}
+
+export default App;
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   const [data, setData] = useState(null);
+//
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:3000/api/data'); // Replace with your backend URL
+//         setData(response.data);
+//       } catch (error) {
+//         console.error('Error fetching data:', error);
+//       }
+//     };
+//
+//     fetchData();
+//   }, []);
+//
+//   return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           {data ? (
+//               <p>Data from backend: {data.nahhCount}</p>
+//           ) : (
+//               <p>Loading...</p>
+//           )}
+//           <a
+//               className="App-link"
+//               href="https://reactjs.org"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//   );
+// }
+//
+// export default App;
---
+++ trendingdata/src/components/T1Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import T1LogoImage from '../images/T1_Logo.jpg';
+
+const T1Logo= () => {
+
+    return (
+
+        <img
+            src={T1LogoImage}
+            alt="Picture of T1 Logo"
+            style={{
+        width: '200px',
+        height: 'auto',
+        }}
+        />
+    );
+};
+export default T1Logo;
+
+
+
